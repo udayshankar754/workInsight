06f78711bcbb5bdfdd31f036135adfe62e42b643
Ghanshayam Kumar Giet <ghanshyam.kumar.edu@gmail.com>
2025-02-20 22:30:35 +0530

    added delete user

--- src/controllers/user_admin.controllers.js
+++ src/controllers/user_admin.controllers.js
@@ -254,7 +254,16 @@ const updatePassword = asyncHandler(async (req, res) => {});
 
 const markAsDeleteAccount = asyncHandler(async (req, res) => {});
 
-const deleteUser = asyncHandler(async (req, res) => {});
+const deleteUser = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  const user = await User.findByIdAndDelete(userId);
+  if (!user) {
+    throw new ApiError(404, 'User not found');
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, 'User deleted successfully'));
+});
 
 const getUserList = asyncHandler(async (req, res) => {});
 
